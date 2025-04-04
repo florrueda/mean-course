36f21046cb4592475a65d5e1df345cb1f5533537
Florencia Rueda <ana.rueda@accenture.com>
2025-04-04 18:58:13 -0300

    add routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
-  templateUrl: './header.component.html'
+  imports: [MatToolbarModule, RouterLink, MatButtonModule, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
